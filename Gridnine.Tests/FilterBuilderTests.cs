e3abc6061016b828e42fe9e115e236e1a6f09037
Sergei3190 <sergkosya@mail.ru>
2021-04-15 01:50:58 +0300

    добавлена библиотека тестов для Gridnine

---
+++ Gridnine.Tests/FilterBuilderTests.cs
@@ -0,0 +1,77 @@
+﻿using Gridnine.FlightCodingTest;
+using Gridnine.Tests.TestData;
+using Xunit;
+
+namespace Gridnine.Tests
+{
+    public class FilterBuilderTests
+    {
+        [Fact]
+        public void GetFilters_Ok()
+        {
+            var filterBuilder = new FilterBuilder();
+
+            var result = filterBuilder.GetFilters();
+
+            Assert.NotEmpty(result);
+        }
+
+        [Fact]
+        public void AddFilter_Ok()
+        {
+            var filterBuilder = new FilterBuilder();
+
+            var oldCount = filterBuilder.GetFilters().Count;
+
+            filterBuilder.AddFilter(FilterBuilderTestData.GoodNewFilter);
+
+            var newCount = filterBuilder.GetFilters().Count;
+
+            Assert.NotEqual<int>(oldCount, newCount);
+        }
+
+        [Fact]
+        public void AddFilter_Bad()
+        {
+            var filterBuilder = new FilterBuilder();
+
+            var oldCount = filterBuilder.GetFilters().Count;
+
+            filterBuilder.AddFilter(FilterBuilderTestData.BadNewFilter);
+
+            var newCount = filterBuilder.GetFilters().Count;
+
+            Assert.Equal<int>(oldCount, newCount);
+        }
+
+        [Fact]
+        public void RemoveFilter_Ok()
+        {
+            var filterBuilder = new FilterBuilder();
+
+            var oldCount = filterBuilder.GetFilters().Count;
+
+            var result = filterBuilder.RemoveFilter(3);
+
+            var newCount = filterBuilder.GetFilters().Count;
+
+            Assert.NotEqual<int>(oldCount, newCount);
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void RemoveFilter_Exception()
+        {
+            var filterBuilder = new FilterBuilder();
+
+            var oldCount = filterBuilder.GetFilters().Count;
+
+            var result = filterBuilder.RemoveFilter(5);
+
+            var newCount = filterBuilder.GetFilters().Count;
+
+            Assert.Equal<int>(oldCount, newCount);
+            Assert.False(result);
+        }
+    }
+}
